2cf424ea8952215f40e48f26901f2d53da64ec93
Nicolò Tellini <nicolo.tellini@unice.fr>
2025-10-29 15:11:29 +0100

    Refactor configuration and pipeline steps in runner.sh
    Refactor user configuration section and streamline pipeline steps.

--- runner.sh
+++ runner.sh
@@ -1,308 +1,228 @@
 #!/bin/bash
 
 ############################
-### User configuration   ###
+# User configuration
 ############################
 
-## Reference assembly
-ref2Label="CBS432"    # S. paradoxus reference assembly label
-ref2="EU"             # Short name for reference
+# References
+ref1Label="Scc"
+ref2Label="CBS432"   # S. paradoxus reference assembly label
+ref2="EU"            # Short name for reference
+
+# Resources
 nSamples=5
 nThreads=8
-indexing="yes"        # Build reference index ("yes","no")
 
-############################
-### Pipeline steps       ###
-############################
+# Switches
+indexing="yes"          # yes|no
+fastqQC="yes"           # yes|no|-
+shortReadMapping="yes"  # yes|no
+mrkgeno="yes"           # yes|no
+cnv="yes"               # yes|no
+intro="yes"             # yes|no
+heatmap="yes"           # yes|no
 
-## STEP 1: Quality control
-fastqQC="yes"          # Run FastQC ("yes","no","-")
-
-## STEP 2: Short-read mapping
-shortReadMapping="yes" # Map reads ("yes","no")
-
-## STEP 3: Marker genotyping
-mrkgeno="yes"          # Genotype markers ("yes","no")
-
-## STEP 4: Copy number variation
-cnv="yes"              # CNV analysis ("yes","no")
-
-## STEP 5: Introgression detection
-intro="yes"            # Introgression analysis ("yes","no")
-
-## STEP 6: Visualization
-heatmap="yes"          # Generate heatmap ("yes","no")
-
-############################
-### End of configuration ###
-############################
+### --- START --- ###
 
 BaseDir=$(pwd)
-
 ref1="Scc"
-
 ref1Label="Scc"
 
 # check logs folder
-if [[ ! -d ${BaseDir}/scr/logs ]]; then mkdir ${BaseDir}/scr/logs; fi
+if [[ ! -d ${BaseDir}/scr/logs ]]; then
+    mkdir ${BaseDir}/scr/logs
+fi
 
-if [ $indexing == "yes" ]
-then
+if [ $indexing == "yes" ]; then
+    mkdir -p ${BaseDir}/ref/Ann
 
-mkdir -p ${BaseDir}/ref/Ann
+    cp ${BaseDir}/rep/Asm/${ref1Label}.genome.fa ${BaseDir}/ref &
+    cp ${BaseDir}/rep/Asm/${ref2Label}.genome.fa ${BaseDir}/ref &
+    cp ${BaseDir}/rep/Ann/${ref1Label}.* ${BaseDir}/ref/Ann &
+    cp ${BaseDir}/rep/Ann/${ref2Label}.* ${BaseDir}/ref/Ann &
+    wait
 
-cp ${BaseDir}/rep/Asm/${ref1Label}.genome.fa ${BaseDir}/ref &
+    echo "Preparing the assemblies..."
 
-cp ${BaseDir}/rep/Asm/${ref2Label}.genome.fa ${BaseDir}/ref &
-
-cp ${BaseDir}/rep/Ann/${ref1Label}.* ${BaseDir}/ref/Ann &
-
-cp ${BaseDir}/rep/Ann/${ref2Label}.* ${BaseDir}/ref/Ann &
-
-wait
-
-echo "Preparing the assemblies..."
-
-for i in $(ls $BaseDir/ref/*fa)
-do
-  genome=$(echo $i | rev | cut -d"/" -f1 | rev | cut -d"." -f1)
-  sed -i "/^>/s/$/_$genome/" $i
-  minimap2 -d $BaseDir/ref/$genome.mmi $i 2>/dev/null
-  # bwa index $i
-  samtools faidx $i 2>/dev/null
-done
-
+    for i in $(ls $BaseDir/ref/*fa); do
+        genome=$(echo $i | rev | cut -d"/" -f1 | rev | cut -d"." -f1)
+        sed -i "/^>/s/$/_$genome/" $i
+        minimap2 -d $BaseDir/ref/$genome.mmi $i 2>/dev/null
+        # bwa index $i
+        samtools faidx $i 2>/dev/null
+    done
 fi
 
 wait
 
-if [ $fastqQC == "no" ] 
-then
-	echo "Preparing fastqs ..."
-	/usr/bin/time -v bash ${BaseDir}/scr/get_reads_len.sh "${BaseDir}" > ${BaseDir}/scr/logs/get_reads_len.out 2> ${BaseDir}/scr/logs/Time.get_reads_len.err
-        wait
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.get_reads_len.err | cut -d":" -f2 | tr -d "[:blank:]")
-	if [ $check == 1 ]
-	then
-	echo "Exit at get_reads_len.sh"
-	exit
-	else
+if [ $fastqQC == "no" ]; then
+    echo "Preparing fastqs ..."
+    /usr/bin/time -v bash ${BaseDir}/scr/get_reads_len.sh "${BaseDir}" > ${BaseDir}/scr/logs/get_reads_len.out 2> ${BaseDir}/scr/logs/Time.get_reads_len.err
+    wait
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.get_reads_len.err | cut -d":" -f2 | tr -d "[:blank:]")
+    if [ $check == 1 ]; then
+        echo "Exit at get_reads_len.sh"
+        exit
+    else
         echo "WARNING: required only read length extraction whitout fastQC report"
-	echo "get_reads_len ... OK"
-	fi
-elif [ $fastqQC == "yes" ]
-then
-	echo "Preparing fastqs ..."
-       	/usr/bin/time -v bash ${BaseDir}/scr/fastQC.sh "${BaseDir}" > ${BaseDir}/scr/logs/fastQC.out 2> ${BaseDir}/scr/logs/Time.fastQC.err
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.fastQC.err | cut -d":" -f2 | tr -d "[:blank:]")
-	if [ $check == 1 ]
-	then
-	echo "Exit at fastQC.sh"
-	exit
-	else
+        echo "get_reads_len ... OK"
+    fi
+
+elif [ $fastqQC == "yes" ]; then
+    echo "Preparing fastqs ..."
+    /usr/bin/time -v bash ${BaseDir}/scr/fastQC.sh "${BaseDir}" > ${BaseDir}/scr/logs/fastQC.out 2> ${BaseDir}/scr/logs/Time.fastQC.err
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.fastQC.err | cut -d":" -f2 | tr -d "[:blank:]")
+    if [ $check == 1 ]; then
+        echo "Exit at fastQC.sh"
+        exit
+    else
         echo "fastQC reports ... OK"
-	fi
-       	wait
-	
-	/usr/bin/time -v bash ${BaseDir}/scr/read_len_QC.sh "${BaseDir}" > ${BaseDir}/scr/logs/read_len_QC.out 2> ${BaseDir}/scr/logs/Time.read_len_QC.err
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.read_len_QC.err | cut -d":" -f2 | tr -d "[:blank:]")
-	if [ $check == 1 ]
-	then
-	echo "Exit at read_len_QC.sh"
-	exit
-	else
+    fi
+    wait
+
+    /usr/bin/time -v bash ${BaseDir}/scr/read_len_QC.sh "${BaseDir}" > ${BaseDir}/scr/logs/read_len_QC.out 2> ${BaseDir}/scr/logs/Time.read_len_QC.err
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.read_len_QC.err | cut -d":" -f2 | tr -d "[:blank:]")
+    if [ $check == 1 ]; then
+        echo "Exit at read_len_QC.sh"
+        exit
+    else
         echo "read_len_QC ... OK"
-	fi
-elif [ $fastqQC == "-" ]
-then
-	echo "WARNING: read length extraction AND fastQC report ... skipped"
+    fi
+
+elif [ $fastqQC == "-" ]; then
+    echo "WARNING: read length extraction AND fastQC report ... skipped"
 fi
 
 wait
 
-if [ $shortReadMapping == "yes" ] 
-then
-	echo "short-read mapping ..."
-
-	/usr/bin/time -v bash ${BaseDir}/scr/minimap2.sh "${BaseDir}" "${nSamples}" "${nThreads}" > ${BaseDir}/scr/logs/minimap2.out 2> ${BaseDir}/scr/logs/Time.minimap2.err
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.minimap2.err | cut -d":" -f2 | tr -d "[:blank:]")
-	failed=$(grep failed ${BaseDir}/scr/logs/Time.minimap2.err)
-	if [ $check == 1 ]
-	then
-	echo "Exit at minimap2.sh"
-	exit
-        elif [[ ! -z "$failed" ]]
-        then	
-	echo "bwa failed steps, take a look to scr/logs/Time.minimpa2.err"
+if [ $shortReadMapping == "yes" ]; then
+    echo "short-read mapping ..."
+    /usr/bin/time -v bash ${BaseDir}/scr/minimap2.sh "${BaseDir}" "${nSamples}" "${nThreads}" > ${BaseDir}/scr/logs/minimap2.out 2> ${BaseDir}/scr/logs/Time.minimap2.err
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.minimap2.err | cut -d":" -f2 | tr -d "[:blank:]")
+    failed=$(grep failed ${BaseDir}/scr/logs/Time.minimap2.err)
+    if [ $check == 1 ]; then
+        echo "Exit at minimap2.sh"
         exit
-	else
-	echo "minimap2 mapping ... OK"	
-	fi
-
-else
-echo "BWA mapping ... skipped"
-fi
-
-wait
-
-if [ $mrkgeno == "yes" ] 
-then
-	echo "Genotyping markers ..."
-        /usr/bin/time -v bash ${BaseDir}/scr/bcftools_marker.sh "${ref1Label}" "${ref2Label}" "${BaseDir}" "${nSamples}" "${nThreads}" > ${BaseDir}/scr/logs/bcftools_marker.out 2> ${BaseDir}/scr/logs/Time.bcftools_marker.err
-        /usr/bin/time -v Rscript ${BaseDir}/scr/parser_marker.r "${BaseDir}" > ${BaseDir}/scr/logs/parser_marker.out 2> ${BaseDir}/scr/logs/Time.parser_marker.err
-	wait
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.bcftools_marker.err | cut -d":" -f2 | tr -d "[:blank:]")
-	failed=$(grep failed ${BaseDir}/scr/logs/Time.bcftools_marker.err)
-	if [ $check == 1 ]
-	then
-	echo "Exit at bcftools_marker.sh"
-	exit
-        elif [[ ! -z "$failed" ]]
-        then	
-	echo "bcftools mpileup/call failed steps, take a look to scr/logs/Time.bcftools_marker.err"
+    elif [[ ! -z "$failed" ]]; then
+        echo "bwa failed steps, take a look to scr/logs/Time.minimpa2.err"
         exit
-	else
-	echo "markers geno ... OK"	
-	fi
+    else
+        echo "minimap2 mapping ... OK"
+    fi
 else
-echo "markers geno ... skipped"
+    echo "BWA mapping ... skipped"
 fi
 
 wait
 
-if [ $cnv == "yes" ]
-then
-	echo "Preparing CNVs ..."
-        /usr/bin/time -v bash ${BaseDir}/scr/gem.sh $ref1Label $ref2Label $BaseDir > ${BaseDir}/scr/logs/gem.out 2> ${BaseDir}/scr/logs/Time.gem.err
+if [ $mrkgeno == "yes" ]; then
+    echo "Genotyping markers ..."
+    /usr/bin/time -v bash ${BaseDir}/scr/bcftools_marker.sh "${ref1Label}" "${ref2Label}" "${BaseDir}" "${nSamples}" "${nThreads}" > ${BaseDir}/scr/logs/bcftools_marker.out 2> ${BaseDir}/scr/logs/Time.bcftools_marker.err
+    /usr/bin/time -v Rscript ${BaseDir}/scr/parser_marker.r "${BaseDir}" > ${BaseDir}/scr/logs/parser_marker.out 2> ${BaseDir}/scr/logs/Time.parser_marker.err
+    wait
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.bcftools_marker.err | cut -d":" -f2 | tr -d "[:blank:]")
+    failed=$(grep failed ${BaseDir}/scr/logs/Time.bcftools_marker.err)
+    if [ $check == 1 ]; then
+        echo "Exit at bcftools_marker.sh"
+        exit
+    elif [[ ! -z "$failed" ]]; then
+        echo "bcftools mpileup/call failed steps, take a look to scr/logs/Time.bcftools_marker.err"
+        exit
+    else
+        echo "markers geno ... OK"
+    fi
+else
+    echo "markers geno ... skipped"
+fi
 
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.gem.err | cut -d":" -f2 | tr -d "[:blank:]")
-        failed=$(grep failed ${BaseDir}/scr/logs/Time.gem.err)
-        if [ $check == 1 ]
-        then
+wait
+
+if [ $cnv == "yes" ]; then
+    echo "Preparing CNVs ..."
+    /usr/bin/time -v bash ${BaseDir}/scr/gem.sh $ref1Label $ref2Label $BaseDir > ${BaseDir}/scr/logs/gem.out 2> ${BaseDir}/scr/logs/Time.gem.err
+
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.gem.err | cut -d":" -f2 | tr -d "[:blank:]")
+    failed=$(grep failed ${BaseDir}/scr/logs/Time.gem.err)
+    if [ $check == 1 ]; then
         echo "Exit at gem.sh"
         exit
-        elif [[ ! -z "$failed" ]]
-        then
+    elif [[ ! -z "$failed" ]]; then
         echo "gem failed, take a look to scr/logs/Time.gem.err"
         exit
-        else
-        echo "Mappability ... OK"       
-        fi
-wait
+    else
+        echo "Mappability ... OK"
+    fi
+    wait
 
-        /usr/bin/time -v bash ${BaseDir}/scr/prefreec.sh $ref1Label $ref2Label $BaseDir > ${BaseDir}/scr/logs/prefreec.out 2> ${BaseDir}/scr/logs/Time.prefreec.err
+    /usr/bin/time -v bash ${BaseDir}/scr/prefreec.sh $ref1Label $ref2Label $BaseDir > ${BaseDir}/scr/logs/prefreec.out 2> ${BaseDir}/scr/logs/Time.prefreec.err
 
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.prefreec.err | cut -d":" -f2 | tr -d "[:blank:]")
-        failed=$(grep failed ${BaseDir}/scr/logs/Time.prefreec.err)
-        if [ $check == 1 ]
-        then
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.prefreec.err | cut -d":" -f2 | tr -d "[:blank:]")
+    failed=$(grep failed ${BaseDir}/scr/logs/Time.prefreec.err)
+    if [ $check == 1 ]; then
         echo "Exit at prefreec.sh.sh"
         exit
-        elif [[ ! -z "$failed" ]]
-        then
+    elif [[ ! -z "$failed" ]]; then
         echo "pre control freec failed, take a look to scr/logs/Time.prefreec.sh.err"
         exit
-        else
-        echo "preFREEC ... OK"       
-        fi
-wait
-echo "Detecting CNVs ..."
-      
-        /usr/bin/time -v bash ${BaseDir}/scr/freec.sh $BaseDir $nThreads > /dev/null 2> ${BaseDir}/scr/logs/Time.freec.err
-        
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.freec.err | cut -d":" -f2 | tr -d "[:blank:]")
-        failed=$(grep failed ${BaseDir}/scr/logs/Time.freec.err)
-        if [ $check == 1 ]
-        then
+    else
+        echo "preFREEC ... OK"
+    fi
+    wait
+
+    echo "Detecting CNVs ..."
+    /usr/bin/time -v bash ${BaseDir}/scr/freec.sh $BaseDir $nThreads > /dev/null 2> ${BaseDir}/scr/logs/Time.freec.err
+
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.freec.err | cut -d":" -f2 | tr -d "[:blank:]")
+    failed=$(grep failed ${BaseDir}/scr/logs/Time.freec.err)
+    if [ $check == 1 ]; then
         echo "Exit at freec.sh"
         exit
-        elif [[ ! -z "$failed" ]]
-        then
+    elif [[ ! -z "$failed" ]]; then
         echo "freec failed, take a look to scr/logs/Time.freec.err"
         exit
-        else
-        echo "FREEC ... OK"       
-        fi
+    else
+        echo "FREEC ... OK"
+    fi
 else
-echo "CNVs ... skipped"
+    echo "CNVs ... skipped"
 fi
 
-if [ $intro == "yes" ] 
-then
-        echo "Detecting segments ..."
-        /usr/bin/time -v Rscript ${BaseDir}/scr/clrs.r $ref1Label $ref2Label $BaseDir > ${BaseDir}/scr/logs/clrs.out 2> ${BaseDir}/scr/logs/Time.clrs.err
-	
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.clrs.err | cut -d":" -f2 | tr -d "[:blank:]")
-	failed=$(grep failed ${BaseDir}/scr/logs/Time.clrs.err)
-	if [ $check == 1 ]
-	then
-	echo "Exit at clrs"
-	exit
-        elif [[ ! -z "$failed" ]]
-        then	
-	echo "clrs failed, take a look to scr/logs/Time.clrs.err"
+if [ $intro == "yes" ]; then
+    echo "Detecting segments ..."
+    /usr/bin/time -v Rscript ${BaseDir}/scr/clrs.r $ref1Label $ref2Label $BaseDir > ${BaseDir}/scr/logs/clrs.out 2> ${BaseDir}/scr/logs/Time.clrs.err
+
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.clrs.err | cut -d":" -f2 | tr -d "[:blank:]")
+    failed=$(grep failed ${BaseDir}/scr/logs/Time.clrs.err)
+    if [ $check == 1 ]; then
+        echo "Exit at clrs"
         exit
-	else
-	echo "clrs ... OK"	
-	fi
-        wait
-	
-	#/usr/bin/time -v Rscript ${BaseDir}/scr/AllSeg.R $ref1Label $ref2Label $ref1 $ref2 $BaseDir > ${BaseDir}/scr/logs/AllSeg.out 2> ${BaseDir}/scr/logs/Time.AllSeg.err
-	
-	#check=$(grep Exit ${BaseDir}/scr/logs/Time.AllSeg.err | cut -d":" -f2 | tr -d "[:blank:]")
-	#failed=$(grep failed ${BaseDir}/scr/logs/Time.AllSeg.err)
-	#if [ $check == 1 ]
-	#then
-	#echo "Exit at AllSeg.R"
-	#exit
-        #elif [[ ! -z "$failed" ]]
-        #then	
-	#echo "AllSeg.R failed, take a look to scr/logs/Time.AllSeg.err"
-        #exit
-	#else
-	#echo "AllSeg ... OK"	
-	#fi
-	#wait
-	
-	#/usr/bin/time -v Rscript ${BaseDir}/scr/intro_allchr_plots.R ${BaseDir}  > ${BaseDir}/scr/logs/intro_allchr_plots.out 2> ${BaseDir}/scr/logs/Time.intro_allchr_plots.err
-	
-	#check=$(grep Exit ${BaseDir}/scr/logs/Time.intro_allchr_plots.err | cut -d":" -f2 | tr -d "[:blank:]")
-	#failed=$(grep failed ${BaseDir}/scr/logs/Time.intro_allchr_plots.err)
-	#if [ $check == 1 ]
-	#then
-	#echo "Exit at intro.allchr.plots.r"
-	#exit
-        #elif [[ ! -z "$failed" ]]
-        #then	
-	#echo "intro failed, take a look to scr/logs/Time.intro.allchr.plots.err"
-        #exit
-	#else
-	#echo "red-blue plots ... OK"	
-	#fi
+    elif [[ ! -z "$failed" ]]; then
+        echo "clrs failed, take a look to scr/logs/Time.clrs.err"
+        exit
+    else
+        echo "clrs ... OK"
+    fi
+    wait
 else
-echo "Inrogression ... skipped"
+    echo "Inrogression ... skipped"
 fi
 
-if [ $heatmap == "yes" ] 
-then
-	
-	/usr/bin/time -v Rscript ${BaseDir}/scr/heatmap_events.R ${BaseDir} ${ref1} > ${BaseDir}/scr/logs/heatmap_events.out 2> ${BaseDir}/scr/logs/Time.heatmap_events.err
-	
-	check=$(grep Exit ${BaseDir}/scr/logs/Time.heatmap_events.err | cut -d":" -f2 | tr -d "[:blank:]")
-	failed=$(grep failed ${BaseDir}/scr/logs/Time.heatmap_events.err)
-	if [ $check == 1 ]
-	then
-	echo "Exit at heatmap_events.R"
-	exit
-        elif [[ ! -z "$failed" ]]
-        then	
-	echo "heatmap failed, take a look to scr/logs/Time.heatmap_events.err"
+if [ $heatmap == "yes" ]; then
+    /usr/bin/time -v Rscript ${BaseDir}/scr/heatmap_events.R ${BaseDir} ${ref1} > ${BaseDir}/scr/logs/heatmap_events.out 2> ${BaseDir}/scr/logs/Time.heatmap_events.err
+
+    check=$(grep Exit ${BaseDir}/scr/logs/Time.heatmap_events.err | cut -d":" -f2 | tr -d "[:blank:]")
+    failed=$(grep failed ${BaseDir}/scr/logs/Time.heatmap_events.err)
+    if [ $check == 1 ]; then
+        echo "Exit at heatmap_events.R"
         exit
-	else
-	echo "Heatmap ... OK"	
-	fi
+    elif [[ ! -z "$failed" ]]; then
+        echo "heatmap failed, take a look to scr/logs/Time.heatmap_events.err"
+        exit
+    else
+        echo "Heatmap ... OK"
+    fi
 else
-echo "Heatmap ... skipped"
+    echo "Heatmap ... skipped"
 fi
 
 echo "Thank you for using the pipeline."
